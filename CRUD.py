49b2a7ac4fd7b63f3df8858970a234d9f7c9b1bf
Nemes <sangawenemes@gmail.com>
2023-12-20 18:19:44 +0300

    added all features of CRUD

--- CRUD.py
+++ CRUD.py
@@ -12,4 +12,32 @@ id integer Primary Key, name text, age text
 )
 """
 self.cur.execute(sql)
-slef.con.commit()
+self.con.commit()
+
+
+#INSERT FUNCTION
+
+def insert(self, name, age):
+    self.cur.execute("insert into employees values (NULL,?,?)", (name,age))
+    self.con.commit()
+
+#FETCH ALL DATA FORM DB
+    def fetch(self):
+        self.cur.execute("SELECT * FROM employees")
+        rows=self.cur.fetchall()
+        #print(rows)
+        return rows
+    
+#DELETE RECORD IN DB
+    def remove(self,id,):
+        self.cur.execute("dlete from employees where id=?",(id,))
+        self.con.commit()
+
+#UPDATE RECORD IN DB
+        def update(self,id, name, age):
+            self.cur.execute("update employees set name=?, age=? where id=?",
+                             (name,age,id))
+            self.con.commit()
+
+            O=Database("Employee.db")
+    
